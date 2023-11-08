35005788987efcc2f04b14efd8f297de0a64dc4e
George <tsintsadze7@gmail.com>
2023-11-08 11:06:26 +0400

    feat: change pages if searchInput is true

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ const appState = {
     posts: [],
     currentPost: [],
     currentPage: 1,
-    // searchValue: '',
-    // maxPages: 10,
-    // totalPages: 0,
+    totalPages: 10,
+    maxPages: 10,
+    searchValue: false,
 
     config,
 };
@@ -42,38 +42,77 @@ function addAndRemoveListeners() {
 
 function loadPosts() {
     if (getShouldSearch(elements.searchInput.value)) {
+        appState.currentPage = 1;
+        document.getElementById('page-number').innerHTML = appState.currentPage;
         appState.currentPost = appState.posts.filter((post) => {
             return (post.title.includes(getShouldSearch(elements.searchInput.value)) || (post.body.includes(getShouldSearch(elements.searchInput.value))))
         })
-        return showPosts(appState.currentPost);
+        appState.totalPages = Math.ceil(appState.currentPost.length / appState.config.maxPostsPerPage);
+        appState.searchValue = true;
+        showPosts(appState.currentPost);
     } else {
+        appState.totalPages = appState.maxPages
+        appState.searchValue = false;
         appState.currentPost = appState.posts.filter((post) => post.userId === appState.currentPage)
-        return showPosts(appState.currentPost);
+        showPosts(appState.currentPost);
     }
 }
 
 function loadPreviousPage() {
-    if (appState.currentPage > 1) {
-        appState.currentPage -= 1;
-        document.getElementById('page-number').innerHTML = appState.currentPage;
-        loadPosts();
+    if (appState.searchValue) {
+        if (appState.currentPage > 1) {
+            appState.currentPage -= 1;
+            const startIndex = (appState.currentPage - 1) * appState.config.maxPostsPerPage;
+            const endIndex = startIndex + appState.config.maxPostsPerPage;
+            const postsForPage = appState.currentPost.slice(startIndex, endIndex);
+            document.getElementById('page-number').innerHTML = appState.currentPage;
+            showPosts(postsForPage);
+
+        }
+
+    } else {
+        appState.maxPages = 10
+        if (appState.currentPage > 1) {
+            appState.currentPage -= 1;
+            document.getElementById('page-number').innerHTML = appState.currentPage;
+            const userId = appState.currentPage;
+            const postsForPage = appState.posts.filter(post => post.userId === userId);
+            showPosts(postsForPage);
+        }
     }
 }
 
 function loadNextPage() {
-    if (appState.currentPage < 10) {
-        appState.currentPage += 1;
-        document.getElementById('page-number').innerHTML = appState.currentPage;
-        loadPosts();
+    if (appState.searchValue) {
+        appState.maxPages = appState.totalPages
+        const currentLength = appState.currentPost.length;
+        if (currentLength < appState.totalPages * appState.config.maxPostsPerPage) {
+            if (appState.currentPage < appState.maxPages) {
+                const startIndex = appState.currentPage * appState.config.maxPostsPerPage;
+                const endIndex = startIndex + appState.config.maxPostsPerPage;
+                const postsForPage = appState.currentPost.slice(startIndex, endIndex);
+                appState.currentPage += 1;
+                document.getElementById('page-number').innerHTML = appState.currentPage;
+                showPosts(postsForPage);
+            }
+        }
+    } else {
+        appState.maxPages = 10
+        if (appState.currentPage < appState.maxPages) {
+            appState.currentPage += 1;
+            document.getElementById('page-number').innerHTML = appState.currentPage;
+            const userId = appState.currentPage;
+            const postsForPage = appState.posts.filter(post => post.userId === userId);
+            showPosts(postsForPage);
+        }
     }
 }
 
-function showPosts(userPosts) {
+function showPosts(posts) {
     elements.postsContainer.innerHTML = '';
-
     for (let i = 0; i < appState.config.maxPostsPerPage; i += 1) {
-        if (userPosts[i]) {
-            const { id, title, body } = userPosts[i];
+        if (posts[i]) {
+            const { id, title, body } = posts[i];
             const postCard = createPostCard({ id, title, body });
             elements.postsContainer.appendChild(postCard);
         }
